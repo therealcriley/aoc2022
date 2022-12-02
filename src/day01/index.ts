19e64d71324f06428c759492949f8ee8bb914e11
Christopher Riley <betterth@gmail.com>
2022-12-02 17:51:06 -0500

    part 2 solved

--- src/day01/index.ts
+++ src/day01/index.ts
@@ -3,7 +3,7 @@ import run from "aocrunner";
 const parseInput = (rawInput: string) => rawInput;
 
 type Elf = {
-  calorieList: number[];
+  tempCalorieList: number[];
   total: number;
 };
 
@@ -11,38 +11,65 @@ const part1 = (rawInput: string) => {
   const input = parseInput(rawInput).split(/\r?\n/);
 
   const elfList: Elf[] = [];
-  let calorieList: number[] = [];
+  let tempCalorieList: number[] = [];
   let calorieTotalMax: number | undefined;
 
   for (const line of input) {
     const currentCalorieValue = parseInt(line);
     // on blank lines, stop processing an elf and save
     if (line === "") {
-      const total = calorieList.reduce((prev, current) => prev + current);
+      const total = tempCalorieList.reduce((prev, current) => prev + current);
       // For part 1, keep track of who has the most
       if (!calorieTotalMax || total > calorieTotalMax) {
         calorieTotalMax = total;
       }
       // Create an elf based on the data
       const newElf: Elf = {
-        calorieList,
+        tempCalorieList,
         total,
       };
       elfList.push(newElf);
-      calorieList = [];
+      tempCalorieList = [];
       continue;
     }
     // else, keep process elf lines
-    calorieList.push(currentCalorieValue);
+    tempCalorieList.push(currentCalorieValue);
   }
-  console.log(elfList, calorieTotalMax);
+  // console.log(elfList, calorieTotalMax);
   return calorieTotalMax;
 };
 
 const part2 = (rawInput: string) => {
-  const input = parseInput(rawInput);
+  const input = parseInput(rawInput).split(/\r?\n/);
 
-  return;
+  const elfList: Elf[] = [];
+  let tempCalorieList: number[] = [];
+  const calorieTotalList: number[] = [];
+
+  for (const line of input) {
+    const currentCalorieValue = parseInt(line);
+    // on blank lines, stop processing an elf and save
+    if (line === "") {
+      const total = tempCalorieList.reduce((prev, current) => prev + current);
+      // Create an elf based on the data
+      const newElf: Elf = {
+        tempCalorieList,
+        total,
+      };
+      calorieTotalList.push(total);
+      elfList.push(newElf);
+      tempCalorieList = [];
+      continue;
+    }
+    // else, keep process elf lines
+    tempCalorieList.push(currentCalorieValue);
+  }
+  // descending sort calorie totals
+  calorieTotalList.sort((a, b) => b - a);
+
+  // answer is top 3 summed.
+
+  return calorieTotalList[0] + calorieTotalList[1] + calorieTotalList[2];
 };
 
 run({
