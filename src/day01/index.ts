73af1c8ad75bf89953f5adfaefbe7bc2b8b32618
Christopher Riley <betterth@gmail.com>
2022-12-02 17:44:35 -0500

    part 1 solved

--- src/day01/index.ts
+++ src/day01/index.ts
@@ -2,10 +2,41 @@ import run from "aocrunner";
 
 const parseInput = (rawInput: string) => rawInput;
 
-const part1 = (rawInput: string) => {
-  const input = parseInput(rawInput);
+type Elf = {
+  calorieList: number[];
+  total: number;
+};
 
-  return;
+const part1 = (rawInput: string) => {
+  const input = parseInput(rawInput).split(/\r?\n/);
+
+  const elfList: Elf[] = [];
+  let calorieList: number[] = [];
+  let calorieTotalMax: number | undefined;
+
+  for (const line of input) {
+    const currentCalorieValue = parseInt(line);
+    // on blank lines, stop processing an elf and save
+    if (line === "") {
+      const total = calorieList.reduce((prev, current) => prev + current);
+      // For part 1, keep track of who has the most
+      if (!calorieTotalMax || total > calorieTotalMax) {
+        calorieTotalMax = total;
+      }
+      // Create an elf based on the data
+      const newElf: Elf = {
+        calorieList,
+        total,
+      };
+      elfList.push(newElf);
+      calorieList = [];
+      continue;
+    }
+    // else, keep process elf lines
+    calorieList.push(currentCalorieValue);
+  }
+  console.log(elfList, calorieTotalMax);
+  return calorieTotalMax;
 };
 
 const part2 = (rawInput: string) => {
