fb75ad6c5d9a85da1c3cabc58572591731c81935
Christopher Riley <betterth@gmail.com>
2022-12-02 18:08:21 -0500

    day 2 underway

--- src/day02/index.ts
+++ src/day02/index.ts
@@ -1,6 +1,19 @@
 import run from "aocrunner";
 
-const parseInput = (rawInput: string) => rawInput;
+const moveList = { rock: 1, paper: 2, scissors: 3 } as const;
+
+type MoveLineType = [elfMove: string, playerMove: string];
+
+const parseInput = (rawInput: string) => {
+  const parsedInput = rawInput.split(/\r?\n/).map((line) => {
+    const lineArray = line.split("");
+    const elfMove = lineArray[0];
+    const playerMove = lineArray[2];
+    return line;
+  });
+
+  return parsedInput;
+};
 
 const part1 = (rawInput: string) => {
   const input = parseInput(rawInput);
