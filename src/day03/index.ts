8250700ce7ee705ee483d4c54335e9822ad2fb9c
Christopher Riley <betterth@gmail.com>
2022-12-04 11:26:29 -0500

    day 3 - 1 start

--- src/day03/index.ts
+++ src/day03/index.ts
@@ -1,10 +1,30 @@
 import run from "aocrunner";
 
-const parseInput = (rawInput: string) => rawInput;
+// Sack = 2 compartments
+
+// Every item type is identified by a single lowercase or uppercase letter
+//  (that is, a and A refer to different types of items).
+
+//
+
+// The list of items for each rucksack is given as characters all on a single line.
+// A given rucksack always has the same number of items in each of its two compartments,
+// so the first half of the characters represent items in the first compartment,
+// while the second half of the characters represent items in the second compartment.
+
+// Split input in half (2 compartments)
+// Priority:
+//   - Lowercase item types a through z have priorities 1 through 26.
+//   - Uppercase item types A through Z have priorities 27 through 52.
+const parseInput = (rawInput: string) => {
+  return rawInput.split(/\r?\n/).map(() => {});
+};
 
 const part1 = (rawInput: string) => {
   const input = parseInput(rawInput);
 
+  // Find the item type that appears in both compartments of each rucksack.
+  // What is the sum of the priorities of those item types?
   return;
 };
 
